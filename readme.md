1757118d9d5c754a0f65ec0b43f21f0ce6a6a2ee
王爱彬 <hungeraibin@126.com>
2018-03-13 13:26:11 +0800

    工程化搭建 上

---
+++ readme.md
@@ -0,0 +1,24 @@
+# project-fe
+
+a boilerplate for fe architecture
+
+# main functionalities
+
+- npm scripts [dev product]
+
+- babel [ES6 ES5]
+
+- webpack [图片、css、js等处理]
+
+- jest [单元测试]
+
+- vue/react 框架
+
+# file structure 
+
+|- src
+|- |- app
+|- |- components
+|- |- global
+|- |- |- reset.css
+
---
+++ webpack/base.config.js
@@ -0,0 +1,63 @@
+const path = require('path') // 路径模块
+const entry = require('./entry')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(process.cwd(), "src/app"), // webapck编译上下文
+
+  entry: entry,
+
+  watch: true,
+
+  output: {
+    path: path.resolve(process.cwd(), "dist"), // cwd 代表nodejs启动目录
+    filename: "[name].js" // 导出文件名
+  },
+  plugins: [
+    new ExtractTextPlugin("css/[name].css"), // 使用插件 样式放css目录
+    new HtmlWebpackPlugin({ // 引入资源生成html文件 
+      // title: 'sale',
+      template: '../base/webpack.template.html',
+      inject: true
+      // chunks: ['sale'], // 中间资源 对应 js css 为 sale
+      // filename: 'sale.html'
+    })
+  ],
+  module: { // 配置模块
+    rules: [ // 每个规则都是一个对象
+      {
+        test: /\.js$/, // 正则匹配js
+        loader: 'babel-loader' // babel-loader去处理
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'less-loader']
+        })
+      },
+      {
+        test: /\.(png|jpg|gif|woff|woff2|ttf|eot|svg|swf)$/,
+        loader: "file-loader",
+        options: {
+          name: 'assets/[name]_[sha512:hash:base64:7].[ext]' // 图片放assets目录
+        }
+      }
+    ]
+  }
+}
